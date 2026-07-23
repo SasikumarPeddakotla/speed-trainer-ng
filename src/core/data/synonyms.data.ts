b4e155df32c32d24e52254787b83294347a1f67e
SasikumarPeddakotla <peddakotlasasikumar@gmail.com>
2026-07-23 20:25:45 +0530

    Add telugu meaning in synonyms reveal answer

--- src/core/data/synonyms.data.ts
+++ src/core/data/synonyms.data.ts
@@ -3,27 +3,28 @@ import { Synonym } from '../models/synonym.model';
 export const SYNONYMS: Synonym[] = [
   {
     word: 'Lucidity',
-    meaning: 'The quality of being clearly expressed and understood.',
+    meaning: 'The quality of being clearly expressed and understood. - స్పష్టత',
     synonyms: ['Clarity'],
   },
   {
     word: 'Indict',
-    meaning: 'To officially charge somebody with a crime.',
+    meaning: 'To officially charge somebody with a crime. - నేరం ఆరోపించు',
     synonyms: ['Charge'],
   },
   {
     word: 'Appraise',
-    meaning: "To make a formal judgement about the value of a person's work.",
+    meaning:
+      "To make a formal judgement about the value of a person's work. - విలువ కట్టు",
     synonyms: ['Judge'],
   },
   {
     word: 'Deluge',
-    meaning: 'A sudden very heavy fall of rain; flood.',
+    meaning: 'A sudden very heavy fall of rain; flood. - భారీ వర్శపాతము',
     synonyms: ['Flood'],
   },
   {
     word: 'Preponderance',
-    meaning: 'Being larger in number or more important.',
+    meaning: 'Being larger in number or more important. - ప్రాబల్యం',
     synonyms: ['Dominance'],
   },
 ];
