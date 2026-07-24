0b6df0e4792cbae6aa0a0d03652bd1cb6a6ec7cc
Varshitha_Kancharla <kvarshitha20@gmail.com>
2026-07-24 19:45:16 +0530

    upto 58 idioms

--- src/core/data/idioms.data.ts
+++ src/core/data/idioms.data.ts
@@ -22,4 +22,220 @@ export const IDIOMS: Idiom[] = [
     idiom: 'Of the first water',
     meaning: 'Of the best quality.',
   },
+  {
+    idiom: 'Weather the storm',
+    meaning: 'To endure/survive a difficult situation or challenge.',
+  },
+  {
+    idiom: 'Spin your wheels',
+    meaning: 'To waste time or make no progress.',
+  },
+  {
+    idiom: 'Wear the green willow',
+    meaning: 'suffer from unrequited love or sorrow.',
+  },
+  {
+    idiom: 'Give it a whirl',
+    meaning: 'To try something out or give it a chance.',
+  },
+  {
+    idiom: 'Up in the air(about someone/something)',
+    meaning: 'In an uncertain or undecided state.',
+  },
+  {
+    idiom: 'Give oneself airs',
+    meaning: 'Pretend to be more important or superior than one really is.',
+  },
+  {
+    idiom: 'Alphabet soup',
+    meaning: 'A confusing mixture of abbreviations or acronyms.',
+  },
+  {
+    idiom: 'Whoop it up',
+    meaning: 'To enjoy oneself in a noisy and lively way, usually in a group.',
+  },
+  {
+    idiom: 'Be all one to',
+    meaning: 'Make no difference to; be indifferent to.',
+  },
+  {
+    idiom: 'An article of faith',
+    meaning: 'A belief that is accepted without proof or evidence.',
+  },
+  {
+    idiom: 'In the antecedent',
+    meaning: 'Rising in power or influence; in the lead.',
+  },
+  {
+    idiom: 'Bring home the bacon',
+    meaning: 'To earn a living or provide for one\'s family.',
+  },
+  {
+    idiom: 'Banana oil',
+    meaning: 'Nonsense; insincere or exaggerated talk.',
+  },
+  {
+    idiom: 'At large',
+    meaning: 'A criminal escaping from custody or not yet captured;',
+  },
+  {
+    idiom: 'Head over heels',
+    meaning: 'Madly in love; completely infatuated.',
+  },
+  {
+    idiom: 'Keep your wig on',
+    meaning: 'To stay calm and composed, especially in a difficult situation.',
+  },
+  {
+    idiom: 'Pour cold water on',
+    meaning: 'To discourage or criticize someone\'s ideas or plans.',
+  },
+  {
+    idiom: 'A hard nut to crack',
+    meaning: 'A difficult problem or person to deal with.',
+  },
+  {
+    idiom: 'See eye to eye',
+    meaning: 'Be in complete agreement or have the same opinion.',
+  },
+  {
+    idiom: 'Cat Nap',
+    meaning: 'To take a short sleep or rest.',
+  },
+  {
+    idiom: 'Couch potato',
+    meaning: 'A lazy person who spends a lot of time sitting or lying down.',
+  },
+  {
+    idiom: 'Take exception to',
+    meaning: 'To object strongly',
+  },
+  {
+    idiom: 'Cut to the chase',
+    meaning: 'To get to the point quickly or directly.',
+  },
+  {
+    idiom: 'To run out of steam',
+    meaning: 'To lose energy or enthusiasm and stop doing something.',
+  },
+  {
+    idiom: 'To pick holes in',
+    meaning: 'To find faults or criticize something in a petty way.',
+  },
+  {
+    idiom: 'Smell a rat',
+    meaning: 'To suspect that something is wrong or that someone is being deceptive.',
+  },
+  {
+    idiom: 'Live high on the hog',
+    meaning: 'To live in luxury or indulge in pleasures.',
+  },
+  {
+    idiom: 'Flog a dead horse',
+    meaning: 'To continue doing something that is pointless or futile.',
+  },
+  {
+    idiom: 'As fit as a fiddle',
+    meaning: 'To be in excellent health or condition.',
+  },
+  {
+    idiom: 'Argus-eyed',
+    meaning: 'To be very watchful or alert; Observant.',
+  },
+  {
+    idiom: 'To bid defiance',
+    meaning: 'To openly resist or challenge authority or rules/To disregard or ignore something.',
+  },
+  {
+    idiom: 'With bated breath',
+    meaning: 'In an anxious or eager state.',
+  },
+  {
+    idiom: 'To clip one\'s wings',
+    meaning: 'To restrict someone\'s freedom or opportunities.',
+  },
+  {
+    idiom: 'Cloven hoof',
+    meaning: 'The evil intention.',
+  },
+  {
+    idiom: 'Draconian law',
+    meaning: 'A law that is extremely harsh or severe.',
+  },
+  {
+    idiom: 'To fall flat',
+    meaning: 'To have no effect or success.',
+  },
+  {
+    idiom: 'To gird up the loins',
+    meaning: 'To prepare oneself for a difficult task or challenge.',
+  },
+  {
+    idiom: 'At somebody\'s expense',
+    meaning: 'To suffer the consequences or costs of someone else\'s actions.',
+  },
+  {
+    idiom: 'Be in force',
+    meaning: 'In great strength or numbers.',
+  },
+  {
+    idiom: 'Make a scene',
+    meaning: 'To cause a disturbance or make a big deal out of something.',
+  },
+  {
+    idiom: 'Be left stranded',
+    meaning: 'To be left in a difficult situation with no way out.',
+  },
+  {
+    idiom: 'All thumbs',
+    meaning: 'To be clumsy or awkward especially with one\'s hands.',
+  },
+  {
+    idiom: 'A ray of hope',
+    meaning: 'A sign of improvement or a positive development in a difficult situation.',
+  }, 
+  {
+    idiom: 'Be in two minds',
+    meaning: 'To be uncertain or indecisive about something.',
+  },
+  {
+    idiom: 'Wet behind the ears',
+    meaning: 'To be inexperienced or naive.',
+  },
+  {
+    idiom: 'Be on the air',
+    meaning: 'To broadcast on television or radio.',
+  },
+  {
+    idiom: 'Diamond in the rough',
+    meaning: 'A person or thing with great potential that has not yet been developed or recognized or lacking training.',
+  },
+  {
+    idiom: 'A drop in the ocean',
+    meaning: 'A very small amount compared to what is needed or expected.',
+  },
+  {
+    idiom: 'Count one\'s blessings',
+    meaning: 'To recognize and appreciate the good things in one\'s life.',
+  },
+  {
+    idiom: 'Eyebrow-raising',
+    meaning: 'To cause surprise or disapproval.',
+  },
+  {
+    idiom: 'Add insult to injury',
+    meaning: 'To make a bad situation worse.',
+  },
+  {
+    idiom: 'Tie the knot',
+    meaning: 'To get married.',
+  },
+  {
+    idiom: 'In cahoots with',
+    meaning: 'In partnership or collusion with someone, often for a dishonest purpose.',
+  },
+  {
+    idiom: 'Back seat driver',
+    meaning: 'A person who gives unwanted advice or directions, especially to the driver of a vehicle.',
+  },
 ];
