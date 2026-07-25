5e6c111fcd77f4e914b5d5ef772b5c261e7f5af1
Varshitha_Kancharla <kvarshitha20@gmail.com>
2026-07-25 21:08:19 +0530

    upto 201 idioms

--- src/core/data/idioms.data.ts
+++ src/core/data/idioms.data.ts
@@ -238,4 +238,552 @@ export const IDIOMS: Idiom[] = [
     idiom: 'Back seat driver',
     meaning: 'A person who gives unwanted advice or directions, especially to the driver of a vehicle.',
   },
+  {
+    idiom: 'A black patch',
+    meaning: 'A period of bad luck or misfortune or difficulty.',
+  },
+  {
+    idiom: 'A dime a dozen',
+    meaning: 'Very common and of little value or significance.',
+  },
+  {
+    idiom: 'Come what may',
+    meaning: 'Regardless of what happens; no matter the outcome.',
+  },
+  {
+    idiom: 'Against the clock',
+    meaning: 'Working under time pressure or with a deadline.',
+  },
+  {
+    idiom: 'No dice',
+    meaning: 'Used to refuse a request or indicate that something is not possible.',
+  },
+  {
+    idiom: 'Keep at bay',
+    meaning: 'To prevent someone or something from approaching or causing harm.',
+  },
+  {
+    idiom: 'Ring fencing',
+    meaning: 'Guaranteeing that a certain amount of money is used for a specific purpose and cannot be spent on anything else.',
+  },
+  {
+    idiom: 'Keep your chin up',
+    meaning: 'To remain optimistic and hopeful despite difficulties.',
+  },
+  {
+    idiom: 'Let your hair down',
+    meaning: 'To relax and be more carefree or uninhibited.',
+  },
+  {
+    idiom: 'The last straw',
+    meaning: 'A minor difficulty that pushes someone over the edge or causes a breaking point.',
+  },
+  {
+    idiom: 'New kid on the block',
+    meaning: 'A person who is new to a particular place or situation.',
+  },
+  {
+    idiom: 'Come in handy',
+    meaning: 'Turn out to be useful or convenient.',
+  },
+  {
+    idiom: 'Under the weather',
+    meaning: 'Slightly unwell or in low spirits.',
+  },
+  {
+    idiom: 'Be out of order',
+    meaning: 'To be behaving in a strange or unacceptable way/A device not working properly at all.',
+  },
+  {
+    idiom: 'A sight for sore eyes',
+    meaning: 'Something that is extremely pleasing or comforting to see.',
+  },
+  {
+    idiom: 'To hit the sack',
+    meaning: 'To go to bed or go to sleep.',
+  },
+  {
+    idiom: 'Hocus-pocus',
+    meaning: 'Meaningless talk or activity, often intended to deceive or distract someone.',
+  },
+  {
+    idiom: 'Pass the buck',
+    meaning: 'To shift responsibility for a problem or difficult situation onto someone else.',
+  },
+  {
+    idiom: 'Go belly up',
+    meaning: 'To fail completely or go bankrupt.',
+  },
+  {
+    idiom: 'Cross your fingers',
+    meaning: 'To hope for good luck or a positive outcome.',
+  },
+  {
+    idiom: 'Piece of cake',
+    meaning: 'Something that is very easy to do.',
+  },
+  {
+    idiom: 'Learn the ropes',
+    meaning: 'To learn the basic details of how to do a job or activity.',
+  },
+  {
+    idiom: 'The graveyard shift',
+    meaning: 'A work shift that occurs during the late night or early morning hours, typically when most people are asleep.',
+  },
+  {
+    idiom: 'Call it a day',
+    meaning: 'Decide to stop working for the day or to stop doing something.',
+  },
+  {
+    idiom: 'To hang together',
+    meaning: 'To work or function as a team or group/Remain associated.',
+  },
+  {
+    idiom: 'To turn over a new leaf',
+    meaning: 'To start to act in a better or more responsible way.',
+  },
+  {
+    idiom: 'To bury a hatchet',
+    meaning: 'To make peace with someone after a conflict or disagreement.',
+  },
+  {
+    idiom: 'To judge a book by its cover',
+    meaning: 'To form an opinion about someone or something based solely on their appearance.',
+  },
+  {
+    idiom: 'To take a stock of',
+    meaning: 'To assess or evaluate something carefully; Review.',
+  },
+  {
+    idiom: 'Get out of hand',
+    meaning: 'To become uncontrollable or excessive as of a situation.',
+  },
+  {
+    idiom: 'To drive someone up the wall',
+    meaning: 'To make someone annoyed or frustrated to the point of exasperation.',
+  },
+  {
+    idiom: 'To give the slip',
+    meaning: 'To escape from someone or something, typically in a clever or unexpected way.',
+  },
+  {
+    idiom: 'The whole nine yards',
+    meaning: 'The whole of it; everything possible or available.',
+  },
+  {
+    idiom: 'At daggers drawn',
+    meaning: 'In a state of open conflict or hostility.',
+  },
+  {
+    idiom: 'Pull yourself together',
+    meaning: 'To compose oneself or regain control of one\'s emotions or behavior; Calm down.',
+  },
+  {
+    idiom: 'Costs an arm and a leg',
+    meaning: 'To be very expensive.',
+  },
+  {
+    idiom: 'Get out of control',
+    meaning: 'To become difficult to manage or regulate; To become chaotic or unmanageable.',
+  },
+  {
+    idiom: 'Joke with someone',
+    meaning: 'To make a joke or tease someone in a playful way.',
+  },
+  {
+    idiom: 'Butterflies in the stomach',
+    meaning: 'A feeling of nervousness or excitement, typically before a challenging or important event.',
+  },
+  {
+    idiom: 'Out of the woods',
+    meaning: 'No longer in trouble',
+  },
+  {
+    idiom: 'In the pink',
+    meaning: 'In good health or condition.',
+  },
+  {
+    idiom: 'Lock, stock and barrel',
+    meaning: 'Completely; entirely.',
+  },
+  {
+    idiom: 'On tenterhooks',
+    meaning: 'In a state of anxious suspense or uncertainty.',
+  },
+  {
+    idiom: 'Lend an ear',
+    meaning: 'To give one\'s attention to someone or something.',
+  },
+  {
+    idiom: 'Kicked up a row',
+    meaning: 'To create a disturbance or commotion.',
+  },
+  {
+    idiom: 'Bolt from the blue',
+    meaning: 'A sudden and unexpected disaster or event.',
+  },
+  {
+    idiom: 'Cut to the quick',
+    meaning: 'Hurt intensely.',
+  },
+  {
+    idiom: 'Ended in smoke',
+    meaning: 'To result in nothing or no success.',
+  },
+  {
+    idiom: 'The sword of Damocles',
+    meaning: 'A constant and imminent threat or danger.',
+  },
+  {
+    idiom: 'Blow his top',
+    meaning: 'Become very angry.',
+  },
+  {
+    idiom: 'Speaks volumes',
+    meaning: 'Gives enough proof or evidence to make a point clear.',
+  },
+  {
+    idiom: 'All ears',
+    meaning: 'Completely attentive or interested.',
+  },
+  {
+    idiom: 'Swollen headed',
+    meaning: 'Having an exaggerated sense of self-importance; Arrogant;conceited.',
+  },
+  {
+    idiom: 'Playing with fire',
+    meaning: 'Take a grave risk.',
+  },
+  {
+    idiom: 'Turned a deaf ear',
+    meaning: 'Neglected.',
+  },
+  {
+    idiom: 'Nip in the bud',
+    meaning: 'Dropped at an early stage; Stopped before it becomes a problem.',
+  },
+  {
+    idiom: 'In the running',
+    meaning: 'Contesting the position.',
+  },
+  {
+    idiom: 'Under his nose',
+    meaning: 'Right in front of him.',
+  },
+  {
+    idiom: 'His heart\'s content',
+    meaning: 'Complete satisfaction.',
+  },
+  {
+    idiom: 'Suit you to a T',
+    meaning: 'Fit you perfectly; match you exactly.',
+  },
+  {
+    idiom: 'Foot the bill',
+    meaning: 'To pay for something.',
+  },
+  {
+    idiom: 'To sit on the fence',
+    meaning: 'To remain neutral or undecided about something.',
+  },
+  {
+    idiom: 'Take to one\'s heels',
+    meaning: 'To run away quickly.',
+  },
+  {
+    idiom: 'Be at your wits\' end',
+    meaning: 'To be completely puzzled or unable to think of a solution.',
+  },
+  {
+    idiom: 'Full of beans',
+    meaning: 'Energetic and lively.',
+  },
+  {
+    idiom: 'In the dark',
+    meaning: 'In ignorance or unaware of something.',
+  },
+  {
+    idiom: 'Pie in the sky',
+    meaning: 'An unrealistic or impossible idea.',
+  },
+  {
+    idiom: 'Sell like hot cakes',
+    meaning: 'To sell very quickly or easily.',
+  },
+  {
+    idiom: 'In the same boat',
+    meaning: 'In a similar situation or condition.',
+  },
+  {
+    idiom: 'Talk over',
+    meaning: 'To discuss something with someone.',
+  },
+  {
+    idiom: 'Throw in the towel',
+    meaning: 'Admit defeat or give up.',
+  },
+  {
+    idiom: 'Tit for tat',
+    meaning: 'To respond to an action with a similar action, especially in retaliation.',
+  },
+  {
+    idiom: 'Bit your tongue',
+    meaning: 'Stop yourself from saying something.',
+  },
+  {
+    idiom: 'As clear as mud',
+    meaning: 'Not clear at all; completely confusing.',
+  },
+  {
+    idiom: 'Spill the beans',
+    meaning: 'To reveal a secret or confidential information.',
+  },
+  {
+    idiom: 'The crux of the matter',
+    meaning: 'The critical point.',
+  },
+  {
+    idiom: 'Four corners of the earth',
+    meaning: 'Everywhere; all over the world.',
+  },
+  {
+    idiom: 'hanging by a thread',
+    meaning: 'Be in a dangerous situation.',
+  },
+  {
+    idiom: 'To shake off',
+    meaning: 'To get rid of.',
+  },
+  {
+    idiom: 'Going over one\'s head',
+    meaning: 'To exceed someone\'s understanding or capabilities.',
+  },
+  {
+    idiom: 'Mad as a hatter',
+    meaning: 'Very angry or insane.',
+  },
+  {
+    idiom: 'Feathering one\'s nest',
+    meaning: 'To accumulate wealth or resources for one\'s own benefit.',
+  },
+  {
+    idiom: 'Be an old hat',
+    meaning: 'Be outdated',
+  },
+  {
+    idiom: 'Head in the clouds',
+    meaning: 'To daydream.',
+  },
+  {
+    idiom: 'Green thumb',
+    meaning: 'A talent for gardening or growing plants.',
+  },
+  {
+    idiom: 'To grease the palm',
+    meaning: 'To bribe someone.',
+  },
+  {
+    idiom: 'To bark up the wrong tree',
+    meaning: 'To pursue a mistaken or incorrect course of action.',
+  },
+  {
+    idiom: 'To take the bull by the horns',
+    meaning: 'To face danger boldly.',
+  },
+  {
+    idiom: 'Gift of the gab',
+    meaning: 'Have a talent for talking and persuading people.',
+  },
+  {
+    idiom: 'Turn a deaf ear',
+    meaning: 'To neglect or ignore.',
+  },
+  {
+    idiom: 'Take to task',
+    meaning: 'To criticize or reprimand someone.',
+  },
+  {
+    idiom: 'have one\'s hands full',
+    meaning: 'To be very busy.',
+  },
+  {
+    idiom: 'Face the music',
+    meaning: 'Face the criticism.',
+  },
+  {
+    idiom: 'Pass the hat',
+    meaning: 'To ask people to contribute money.',
+  },
+  {
+    idiom: 'As the crow flies',
+    meaning: 'The shortest distance.',
+  },
+  {
+    idiom: 'In a pickle',
+    meaning: 'In trouble.',
+  },
+  {
+    idiom: 'Red letter day',
+    meaning: 'A very special day.',
+  },
+  {
+    idiom: 'Rule the roost',
+    meaning: 'To be the dominant person in a particular situation or environment.',
+  },
+  {
+    idiom: 'At arm\'s length',
+    meaning: 'At a distance; not too close.',
+  },
+  {
+    idiom: 'Acid test',
+    meaning: 'Definitive proof of truth or falsehood.',
+  },
+  {
+    idiom: 'Dark horse',
+    meaning: 'An unexpected winner or success.',
+  },
+  {
+    idiom: 'Hand in glove',
+    meaning: 'In partnership for something dishonest.',
+  },
+  {
+    idiom: 'Late in the day',
+    meaning: 'Too delayed to be of any use',
+  },
+  {
+    idiom: 'Draw a blank',
+    meaning: 'Be unsuccessful',
+  },
+  {
+    idiom: 'Bolt from the blue',
+    meaning: 'Something sudden that happened unexpectedly.',
+  },
+  {
+    idiom: 'Left-handed compliment',
+    meaning: 'An insulting/negative comment disguised as praise.',
+  },
+  {
+    idiom: 'At snail\'s pace',
+    meaning: 'Do something very slowly',
+  },
+  {
+    idiom: 'A pain in the neck',
+    meaning: 'Irritating',
+  },
+  {
+    idiom: 'Beat around the bush',
+    meaning: 'To avoid taking about what is important',
+  },
+  {
+    idiom: 'Make no headway',
+    meaning: 'Unable to make progress',
+  },
+  {
+    idiom: 'Make a short work of',
+    meaning: 'To deal with, finish, or dispose of someone/something very quickly/handily.',
+  },
+  {
+    idiom: 'In a nutshell',
+    meaning: 'Express very briefly',
+  },
+  {
+    idiom: 'Be a open book',
+    meaning: 'To be easy to understand',
+  },
+  {
+    idiom: 'The ball is in one\'s court',
+    meaning: 'To be responsible for further action',
+  },
+  {
+    idiom: 'Bend over backwards',
+    meaning: 'Make every effort to achieve something; try ones\'s hardest',
+  },
+  {
+    idiom: 'Birds of a same feather',
+    meaning: 'Peaple with similar character or interests',
+  },
+  {
+    idiom: 'To be cat\'s whiskers',
+    meaning: 'To be highly impressive',
+  },
+  {
+    idiom: 'Out of date',
+    meaning: 'Something old-fashioned',
+  },
+  {
+    idiom: 'As a matter of fact',
+    meaning: 'In reality; Actually as it happens.',
+  },
+  {
+    idiom: 'Cut one short',
+    meaning: 'To interrupt someone.',
+  },
+  {
+    idiom: 'To take one\'s coat off',
+    meaning: 'To admire someone',
+  },
+  {
+    idiom: 'Cup of tea',
+    meaning: 'Something one prefers, desires, enjoys or care about.',
+  },
+  {
+    idiom: 'On the tip of the tongue',
+    meaning: 'Readily available in one\'s memory',
+  },
+  {
+    idiom: 'A wild-goose chase',
+    meaning: 'Useless search',
+  },
+  {
+    idiom: 'Be at your wits\'s end',
+    meaning: 'Be overwhelmed with problems',
+  },
+  {
+    idiom: 'Cry over split milk',
+    meaning: 'Regret about something that happened and cannot be changed',
+  },
+  {
+    idiom: 'Far cry from',
+    meaning: 'To be very different from',
+  },
+  {
+    idiom: 'A needle in the haystack',
+    meaning: 'Something that is very difficult to locate',
+  },
+  {
+    idiom: 'With open arms',
+    meaning: 'Very happily and eagerly; warmly',
+  },
+  {
+    idiom: 'Take heart',
+    meaning: 'To feel encouraged',
+  },
+  {
+    idiom: 'Get the green light',
+    meaning: 'To get the permission to go ahead with some actions or tasks',
+  },
+  {
+    idiom: 'Bed of roses',
+    meaning: 'An easy, comfortable situation; an enjoyable state',
+  },
+  {
+    idiom: 'At the drop of the hat',
+    meaning: 'Immediately, instantly',
+  },
+  {
+    idiom: 'Make a beeline off',
+    meaning: 'Move quickly or directly towards something; Go straight to',
+  },
+  {
+    idiom: 'Till the cows from home',
+    meaning: 'For a very long time',
+  },
+  {
+    idiom: 'Touch and go',
+    meaning: 'Uncertain; precarious',
+  },
+  {
+    idiom: 'Hit the stack',
+    meaning: 'Go to bed; retire; go to sleep',
+  },
 ];
