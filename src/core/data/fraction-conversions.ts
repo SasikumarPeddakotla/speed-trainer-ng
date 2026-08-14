c1ee912bc3599c8789098b3e7fbd5a56e1a974a5
SasikumarPeddakotla <peddakotlasasikumar@gmail.com>
2026-08-14 07:28:20 +0530

    Add some more important fraction conversions to the data

--- src/core/data/fraction-conversions.ts
+++ src/core/data/fraction-conversions.ts
@@ -1,26 +1,42 @@
 import { FractionConversion } from '../models/fraction-conversion.model';
 
 export const FRACTION_CONVERSIONS: FractionConversion[] = [
+  // 2
   { fraction: '1/2', percentage: '50', decimal: '0.5' },
+
+  // 3
   { fraction: '1/3', percentage: '33.33', decimal: '0.3333' },
   { fraction: '2/3', percentage: '66.67', decimal: '0.6667' },
 
+  // 4
   { fraction: '1/4', percentage: '25', decimal: '0.25' },
   { fraction: '3/4', percentage: '75', decimal: '0.75' },
 
+  // 5
   { fraction: '1/5', percentage: '20', decimal: '0.2' },
   { fraction: '2/5', percentage: '40', decimal: '0.4' },
   { fraction: '3/5', percentage: '60', decimal: '0.6' },
   { fraction: '4/5', percentage: '80', decimal: '0.8' },
 
+  // 6
   { fraction: '1/6', percentage: '16.67', decimal: '0.1667' },
   { fraction: '5/6', percentage: '83.33', decimal: '0.8333' },
 
+  // 7
+  { fraction: '1/7', percentage: '14.29', decimal: '0.1429' },
+  { fraction: '2/7', percentage: '28.57', decimal: '0.2857' },
+  { fraction: '3/7', percentage: '42.86', decimal: '0.4286' },
+  { fraction: '4/7', percentage: '57.14', decimal: '0.5714' },
+  { fraction: '5/7', percentage: '71.43', decimal: '0.7143' },
+  { fraction: '6/7', percentage: '85.71', decimal: '0.8571' },
+
+  // 8
   { fraction: '1/8', percentage: '12.5', decimal: '0.125' },
   { fraction: '3/8', percentage: '37.5', decimal: '0.375' },
   { fraction: '5/8', percentage: '62.5', decimal: '0.625' },
   { fraction: '7/8', percentage: '87.5', decimal: '0.875' },
 
+  // 9
   { fraction: '1/9', percentage: '11.11', decimal: '0.1111' },
   { fraction: '2/9', percentage: '22.22', decimal: '0.2222' },
   { fraction: '4/9', percentage: '44.44', decimal: '0.4444' },
@@ -28,15 +44,97 @@ export const FRACTION_CONVERSIONS: FractionConversion[] = [
   { fraction: '7/9', percentage: '77.78', decimal: '0.7778' },
   { fraction: '8/9', percentage: '88.89', decimal: '0.8889' },
 
+  // 10
   { fraction: '1/10', percentage: '10', decimal: '0.1' },
   { fraction: '3/10', percentage: '30', decimal: '0.3' },
   { fraction: '7/10', percentage: '70', decimal: '0.7' },
   { fraction: '9/10', percentage: '90', decimal: '0.9' },
 
-  { fraction: '1/7', percentage: '14.29', decimal: '0.1429' },
-  { fraction: '2/7', percentage: '28.57', decimal: '0.2857' },
-  { fraction: '3/7', percentage: '42.86', decimal: '0.4286' },
-  { fraction: '4/7', percentage: '57.14', decimal: '0.5714' },
-  { fraction: '5/7', percentage: '71.43', decimal: '0.7143' },
-  { fraction: '6/7', percentage: '85.71', decimal: '0.8571' },
+  // 11
+  { fraction: '1/11', percentage: '9.09', decimal: '0.0909' },
+  { fraction: '2/11', percentage: '18.18', decimal: '0.1818' },
+  { fraction: '3/11', percentage: '27.27', decimal: '0.2727' },
+  { fraction: '4/11', percentage: '36.36', decimal: '0.3636' },
+  { fraction: '5/11', percentage: '45.45', decimal: '0.4545' },
+  { fraction: '6/11', percentage: '54.55', decimal: '0.5455' },
+  { fraction: '7/11', percentage: '63.64', decimal: '0.6364' },
+  { fraction: '8/11', percentage: '72.73', decimal: '0.7273' },
+  { fraction: '9/11', percentage: '81.82', decimal: '0.8182' },
+  { fraction: '10/11', percentage: '90.91', decimal: '0.9091' },
+
+  // 12
+  { fraction: '1/12', percentage: '8.33', decimal: '0.0833' },
+  { fraction: '5/12', percentage: '41.67', decimal: '0.4167' },
+  { fraction: '7/12', percentage: '58.33', decimal: '0.5833' },
+  { fraction: '11/12', percentage: '91.67', decimal: '0.9167' },
+
+  // 13
+  { fraction: '1/13', percentage: '7.69', decimal: '0.0769' },
+  { fraction: '2/13', percentage: '15.38', decimal: '0.1538' },
+  { fraction: '3/13', percentage: '23.08', decimal: '0.2308' },
+  { fraction: '4/13', percentage: '30.77', decimal: '0.3077' },
+  { fraction: '5/13', percentage: '38.46', decimal: '0.3846' },
+  { fraction: '6/13', percentage: '46.15', decimal: '0.4615' },
+  { fraction: '7/13', percentage: '53.85', decimal: '0.5385' },
+  { fraction: '8/13', percentage: '61.54', decimal: '0.6154' },
+  { fraction: '9/13', percentage: '69.23', decimal: '0.6923' },
+  { fraction: '10/13', percentage: '76.92', decimal: '0.7692' },
+  { fraction: '11/13', percentage: '84.62', decimal: '0.8462' },
+  { fraction: '12/13', percentage: '92.31', decimal: '0.9231' },
+
+  // 14
+  { fraction: '1/14', percentage: '7.14', decimal: '0.0714' },
+  { fraction: '3/14', percentage: '21.43', decimal: '0.2143' },
+  { fraction: '5/14', percentage: '35.71', decimal: '0.3571' },
+  { fraction: '9/14', percentage: '64.29', decimal: '0.6429' },
+  { fraction: '11/14', percentage: '78.57', decimal: '0.7857' },
+  { fraction: '13/14', percentage: '92.86', decimal: '0.9286' },
+
+  // 15
+  { fraction: '1/15', percentage: '6.67', decimal: '0.0667' },
+  { fraction: '2/15', percentage: '13.33', decimal: '0.1333' },
+  { fraction: '4/15', percentage: '26.67', decimal: '0.2667' },
+  { fraction: '7/15', percentage: '46.67', decimal: '0.4667' },
+  { fraction: '8/15', percentage: '53.33', decimal: '0.5333' },
+  { fraction: '11/15', percentage: '73.33', decimal: '0.7333' },
+  { fraction: '13/15', percentage: '86.67', decimal: '0.8667' },
+  { fraction: '14/15', percentage: '93.33', decimal: '0.9333' },
+
+  // 16
+  { fraction: '1/16', percentage: '6.25', decimal: '0.0625' },
+  { fraction: '3/16', percentage: '18.75', decimal: '0.1875' },
+  { fraction: '5/16', percentage: '31.25', decimal: '0.3125' },
+  { fraction: '7/16', percentage: '43.75', decimal: '0.4375' },
+  { fraction: '9/16', percentage: '56.25', decimal: '0.5625' },
+  { fraction: '11/16', percentage: '68.75', decimal: '0.6875' },
+  { fraction: '13/16', percentage: '81.25', decimal: '0.8125' },
+  { fraction: '15/16', percentage: '93.75', decimal: '0.9375' },
+
+  // 18
+  { fraction: '1/18', percentage: '5.56', decimal: '0.0556' },
+  { fraction: '5/18', percentage: '27.78', decimal: '0.2778' },
+  { fraction: '7/18', percentage: '38.89', decimal: '0.3889' },
+  { fraction: '11/18', percentage: '61.11', decimal: '0.6111' },
+  { fraction: '13/18', percentage: '72.22', decimal: '0.7222' },
+  { fraction: '17/18', percentage: '94.44', decimal: '0.9444' },
+
+  // 20
+  { fraction: '1/20', percentage: '5', decimal: '0.05' },
+  { fraction: '3/20', percentage: '15', decimal: '0.15' },
+  { fraction: '7/20', percentage: '35', decimal: '0.35' },
+  { fraction: '9/20', percentage: '45', decimal: '0.45' },
+  { fraction: '11/20', percentage: '55', decimal: '0.55' },
+  { fraction: '13/20', percentage: '65', decimal: '0.65' },
+  { fraction: '17/20', percentage: '85', decimal: '0.85' },
+  { fraction: '19/20', percentage: '95', decimal: '0.95' },
+
+  // Very Important Exam Shortcuts
+  { fraction: '1/25', percentage: '4', decimal: '0.04' },
+  { fraction: '1/40', percentage: '2.5', decimal: '0.025' },
+  { fraction: '1/50', percentage: '2', decimal: '0.02' },
+  { fraction: '1/80', percentage: '1.25', decimal: '0.0125' },
+  { fraction: '1/100', percentage: '1', decimal: '0.01' },
+  { fraction: '1/125', percentage: '0.8', decimal: '0.008' },
+  { fraction: '1/200', percentage: '0.5', decimal: '0.005' },
+  { fraction: '1/400', percentage: '0.25', decimal: '0.0025' },
 ];
